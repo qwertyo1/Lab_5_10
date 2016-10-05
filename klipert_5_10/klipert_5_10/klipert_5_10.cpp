749a72585d37902a219c5aa0b73330b101b4b864
qwertyo1 <klipert1968@yandex.ru>
2016-10-05 18:49:51 +0600

    Куча ошибок из-за выделений памяти и указателей, надо фиксить

--- klipert_5_10/klipert_5_10/klipert_5_10.cpp
+++ klipert_5_10/klipert_5_10/klipert_5_10.cpp
@@ -1,14 +1,52 @@
-// klipert_5_10.cpp: определяет точку входа для консольного приложения.
-//
-
 #include "stdafx.h"
 
+tree* formTree() {
+	tree *node = new tree;
+	cout << "Какой длины будет список?" << endl;
+	int n;
+	cin >> n;
+	int t;
+	for (int i = 0; i < n; i++)
+	{
+		cout << "[" << i + 1 << "] = ";
+		cin >> t;
+		*node << t;
+	}
+	*node >> 0;
+	node->treeprint(node);
+	return node;
+}
+
 void menu(){
-	tree gg;
+	tree *node = formTree();
+	while (true) {
+		cout << endl << "====== Меню работы с деревом поиска ======" << endl << " [1] Добавить элемент в дерево" << endl << " [2] Удалить элемент из дерева" << endl;
+		switch (_getch()) {
+		case '1': {	cout << "Значение элемента = ";
+			int t;
+			cin >> t;
+			*node << t;
+			node->treeprint(node);
+			break; }
+		case '2': {
+			int t;
+			cin >> t;
+			*node >> t;
+			//node = node->Remove(node, t);
+			node->treeprint(node);
+			break;
+		}
+		default: {exit(0); }
+		}
+	}
 }
 
 int main()
 {
+	setlocale(LC_ALL, "ru");
+	tree* noode = new tree;
+	menu();
+	system("pause");
     return 0;
 }
 
