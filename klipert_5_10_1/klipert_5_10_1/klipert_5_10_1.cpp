df78f378407b369c08a6985e391bfef2dc5de2f1
qwertyo1 <klipert1968@yandex.ru>
2016-10-07 21:07:44 +0600

    Все работает, осталась перегрузка

--- klipert_5_10_1/klipert_5_10_1/klipert_5_10_1.cpp
+++ klipert_5_10_1/klipert_5_10_1/klipert_5_10_1.cpp
@@ -3,6 +3,16 @@
 
 #include "stdafx.h"
 
+void menu(tree t) {
+	while (true) {
+		cout << endl << "====== Меню работы с деревом поиска ======" << endl << " [1] Добавить элемент в дерево" << endl << " [2] Удалить элемент из дерева" << endl;
+		switch (_getch()) {
+		case '1': {	t = *setElementInfo(t, 1);	break; }
+		case '2': { t = *askDeleteElement(t); break; }
+		default: { exit(0); }
+		}
+	}
+}
 
 int main()
 {
@@ -10,7 +20,7 @@ int main()
 	tree t;
 	int n = askTreeSize();
 	t = *setElementInfo(t, n);
-	t = *askDeleteElement(t);
+	menu(t);
 	system("pause");
     return 0;
 }
